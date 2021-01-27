b51f77d82733be159a7afb869ffb54181da1a4b8
Joanna Puzoń <joanna.puzon93@com>
2021-01-27 13:28:18 +0000

    Second exercise

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Joanna";
+        int myAge = 27 ;
+        double height = 1.66;
+        char carClass = 'B';
+        boolean isGoodProgrammer = true;
+        boolean isKodillaCursant = true;
+        boolean isGoodListener = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+        System.out.println(isKodillaCursant);
+        System.out.println(isGoodListener);
+    }
+}

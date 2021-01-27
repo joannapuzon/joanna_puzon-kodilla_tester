5145021c716553d0d5f98663321946478dd60ce8
Joanna Puzoń <joanna.puzon93@com>
2021-01-27 12:30:42 +0000

    Pierwsze zadanie

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {                             // [1]
+    public static void main(String[] args) {// [2]
+        System.out.println("Hello World");            // [3]
+        System.out.println("Joanna");                 // [4]
+        System.out.println(27);                       // [5]
+        System.out.println("P");                      // [6]
+    }
+}
